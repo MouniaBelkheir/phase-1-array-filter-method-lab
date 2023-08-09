662927d12373650cc9b679a7852823414830d6f1
mouniabelkheir <mouniabelkheir@outlook.com>
2023-08-09 20:50:13 +0100

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
+
 // Code your solution here
+const drivers = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'Bobby'];
+
+function findMatching(drivers, searchString) {
+    const searchLower = searchString.toLowerCase();
+    return drivers.filter(driver => driver.toLowerCase() === searchLower);
+  }
+  
+  
+  function fuzzyMatch(drivers, searchString) {
+    const searchLower = searchString.toLowerCase();
+    return drivers.filter(driver => driver.toLowerCase().startsWith(searchLower));
+  }
+  
+  function matchName(drivers, name) {
+    return drivers.filter(driver => driver.name === name);
+  }
+ 
+  
+  
+  
